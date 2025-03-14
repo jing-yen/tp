65cb6dde448d393291457c5c7ad2753856102d29
xiaohan28 <zgxiaohan0628@gmail.com>
2025-03-14 17:30:55 +0800

    Add welcome message and change Command to abstract class

--- src/main/java/paypals/commands/Command.java
+++ src/main/java/paypals/commands/Command.java
@@ -3,16 +3,14 @@ package paypals.commands;
 import paypals.ActivityManager;
 import paypals.exception.PayPalsException;
 
-public class Command {
+public abstract class Command {
     String command;
 
     Command(String command) {
         this.command = command;
     }
 
-    public void execute(ActivityManager activityManager) throws PayPalsException {
-
-    }
+    public abstract void execute(ActivityManager activityManager) throws PayPalsException;
 
     public boolean isExit() {
         return false;
