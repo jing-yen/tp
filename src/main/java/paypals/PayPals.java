e1494c83e61006997593f27196d43dc4e573f016
Jing Yen <tong414782@gmail.com>
2025-03-25 00:09:40 +0800

    Remove emoji

--- src/main/java/paypals/PayPals.java
+++ src/main/java/paypals/PayPals.java
@@ -32,7 +32,7 @@ public class PayPals {
         Logging.logInfo("Entering main program body. Begin accepting user commands");
         while (!isExit) {
             try {
-                System.out.print("💵> ");
+                System.out.print("> ");
                 String fullCommand = ui.readLine();
                 Command c = parser.decodeCommand(fullCommand);
                 c.execute(activityManager, true);
