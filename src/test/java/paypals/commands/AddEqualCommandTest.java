30f2bf6db5016ba16023a3b90103eb76621fa5ec
Jing Yen <tong414782@gmail.com>
2025-04-08 04:36:37 +0800

    checkstyle 2

--- src/test/java/paypals/commands/AddEqualCommandTest.java
+++ src/test/java/paypals/commands/AddEqualCommandTest.java
@@ -9,7 +9,10 @@ import paypals.exception.PayPalsException;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AddEqualCommandTest extends PayPalsTest {
     @Test
